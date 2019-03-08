5bac3ec48dccef41f5ace8c1c85d571cd5602f8f
Rodrigo Vilchez Fernandez <rvilchezf@outlook.com>
2019-03-08 12:32:56 -0300

    Add files via upload
    condicional if

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool hacefrio;
+            hacefrio = true;
+            Console.WriteLine(hacefrio);
+            Console.WriteLine(!hacefrio);
+
+
+
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad, ingresa tu edad");
+            int edad;
+            edad = Int32.Parse(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Adelante, eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("pa juera eres muy pequeño");
+            }
+
+
+        }
+    }
+}
